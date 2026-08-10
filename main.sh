35ee0d754b0e8de0e0553815e151aa84376c92a9
Kaarel <kmaesalu06@gmail.com>
2026-08-10 14:40:51 +0300

    Working script
    copyAndCompress functionality, tar compression, minor tweaks

--- main.sh
+++ main.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Backup Location
+
+# - Drive
+# -- Fedora
+
 declare -A devices_hash # Connected removable lsblk devices and their description pairs.
 devices=() # Indexed and filtered block devices.
 
+BACKUP_FROM=("$HOME/Pictures" "$HOME/Documents" "$HOME/Projects" "$HOME/Music")
 MOUNT_POINT=""
 
-clear -x
+BACKUP_DRIVE=""
+CURRENT_DATE=$(date +%F)
 
-echo "AUTO-BACKUP TOOL"
-echo ""
+ROOT_FOLDER_NAME="Fedora_backup_test"
 
 getConnectedDevices() {
     while read -r line; do
@@ -21,7 +27,7 @@ getConnectedDevices() {
     done < <(lsblk -P -o NAME,SIZE,MOUNTPOINTS,RM)
 }
 
-outputAndIndexDevices() {
+indexAndOutputDevices() {
 	i=0
 	for device in "${!devices_hash[@]}"; do
 	    devices[i]="$device"
@@ -30,20 +36,55 @@ outputAndIndexDevices() {
 	done
 }
 
-navigateToMountPoint() {
+setMountpointAndDrive() {
 	MOUNT_POINT=$(lsblk -no MOUNTPOINTS "${devices[$drive_selection]}")
+	BACKUP_DRIVE="${devices[$drive_selection]}"
+
+	[[ "$BACKUP_DRIVE" == "" ]] && exit 0
+
+}
+
+copyAndCompress() {
+	printf -v backup_locations '%s, ' "${BACKUP_FROM[@]}"
+
+	echo "Drive selected: $BACKUP_DRIVE"
+	echo "Backing up from ${backup_locations%,}. Okay? (y/N)"
+
+	read ok
+	[[ "${ok^^}" != "Y" ]] && echo "Quitting." && exit 0;
+
+	# User has selected to continue
+	# Tar up the selected locations
+
+	for location in "${BACKUP_FROM[@]}"; do
+		
+		IFS='/' read -a array <<< "$location"
+
+		mkdir -p "$MOUNT_POINT/$ROOT_FOLDER_NAME/${array[-1]}/"
+		tar -cJvf "$MOUNT_POINT/$ROOT_FOLDER_NAME/${array[-1]}/$CURRENT_DATE.tar.xz" \
+			--exclude="*.rpp-bak" \
+			--exclude="*.reapeaks" \
+			--exclude="SURGE XT" \
+			"$location"
+
+	done
+
 }
 
 getConnectedDevices
-echo "Select a device..."
-echo ""
 
-outputAndIndexDevices
+#If no suitable devices, exit
+[[ ${#devices_hash[@]} == 0 ]] && echo "Connect backup medium (No medium to backup to!)." && exit 1;
+
+clear -x #Clear view but do not delete history
+
+echo "AUTO-BACKUP TOOL"
+echo "Select a device..."
+
+indexAndOutputDevices
 echo "---"
 
 read drive_selection
-echo ""
 
-navigateToMountPoint
-echo "Device ${devices[$drive_selection]} successfully selected!"
-echo ""
+setMountpointAndDrive
+copyAndCompress
